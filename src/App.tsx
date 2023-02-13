1612c0234d98b7c6d54159f3e9ff1c92347e63d7
Saviovd <savio.pinheiroal@gmail.com>
2023-02-13 14:32:24 -0300

    feat: adição do LightTheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { ThemeProvider } from "@emotion/react";
+import { Light } from "@mui/icons-material";
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
+import { LightTheme } from "./shared/themes";
 
 
 export const App = () => {
   return (
-    <BrowserRouter>
-          <AppRoutes/>
-    </BrowserRouter>
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+            <AppRoutes/>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@mui/material";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 export const AppRoutes = () => {
     return (
         <Routes>  
-            <Route path="/pagina-inicial" element={<div>Pagina inicial</div>}/>
+            <Route path="/pagina-inicial" element={<Button variant='contained' color='primary'>teste</Button>}/>
 
             <Route path="*"  element={<Navigate to="/pagina-inicial"/>}/>
         </Routes>
